59fe332bf4c83e2a837c3d4955a944b1fbb2ded4
atroinina <anastasiyatroinina@gmail.com>
2024-12-01 12:07:08 +0200

    додати process_user_profiles та визвати даг

--- lesson_17/dags/process_user_profiles.py
+++ lesson_17/dags/process_user_profiles.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, coalesce, lit
 
 # ініціалізація DAG
 default_args = {
@@ -18,3 +19,36 @@ dag = DAG(
     schedule_interval=None,  # для ручного запуску
 )
 
+def process_user_profiles():
+    # Шлях до вхідних та вихідних даних
+    input_path = r"C:\Users\small\PycharmProjects\hometasks\lesson_17\data\user_profiles"
+    output_path_silver = r"C:\Users\small\PycharmProjects\hometasks\lesson_17\output\silver"
+
+    # Створення Spark  сесії
+    spark = SparkSession.builder.appName("UserProfileProcessing").getOrCreate()
+
+    # Читання JSONLine фаqлів
+    df = spark.read.json(input_path, multiLine=False)
+
+    # Перетворення даних відповідно до схеми `silver`
+
+    df_cleaned = df.withColumnRenamed("user_id", "client_id") \
+        .withColumnRenamed("first_name", "first_name") \
+        .withColumnRenamed("last_name", "last_name") \
+        .withColumnRenamed("email", "email") \
+        .withColumnRenamed("state", "state") \
+        .withColumnRenamed("age", "age")
+
+    # Зберігаємо дані
+    df_cleaned.write.option("header", "true").json(output_path_silver, mode="overwrite")
+    print(f"Processed and saved user profiles to silver: {output_path_silver}")
+
+    # Закриваємо сесію Spark
+    spark.stop()
+
+
+process_user_profiles_task = PythonOperator(
+    task_id='process_user_profiles',
+    python_callable=process_user_profiles,
+    dag=dag,
+)
