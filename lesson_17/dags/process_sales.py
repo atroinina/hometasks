71a3444b2b17d7af80ade5069ad14a3a99caa8a1
atroinina <anastasiyatroinina@gmail.com>
2024-12-01 10:38:22 +0200

    add process_sales_date() run dag

--- lesson_17/dags/process_sales.py
+++ lesson_17/dags/process_sales.py
@@ -19,3 +19,43 @@ dag = DAG(
     description='Process sales data to bronze',
     schedule_interval=None,  # для ручного запуску
 )
+
+def process_sales_data():
+    # Шлях до вхідних та вихідних даних
+    input_path = "root/data/sales/"
+    output_path = "root/output/bronze/"
+
+    # Створення Spark сесії
+    spark = SparkSession.builder.appName("SalesProcessing").getOrCreate()
+
+    # Перелік всіх файлів в папці
+    files = os.listdir(input_path)
+
+    # Обробка кожного файлу
+    for file in files:
+        if file.endswith(".csv"):
+            # Отримання дати та номера розрізу
+            date, split_number = file.split('__')[0], file.split('__')[1].split('.csv')[0]
+            input_file_path = os.path.join(input_path, file)
+
+            # Читання CSV файлу
+            df = spark.read.csv(input_file_path, header=True, inferSchema=False)
+
+            # Кастуємо всі колонки в STRING
+            df = df.select([col(c).cast("string").alias(c) for c in df.columns])
+
+            # Шлях для збереження результатів
+            output_file_path = os.path.join(output_path, date, f"{date}__{split_number}__sales.csv")
+
+            # Записуємо дані в CSV в папку `bronze`
+            df.write.option("header", "true").csv(output_file_path, mode="overwrite")
+            print(f"Processed and saved: {output_file_path}")
+
+    # Закриваємо сесію Spark
+    spark.stop()
+
+process_sales_task = PythonOperator(
+    task_id='process_sales_to_bronze',
+    python_callable=process_sales_data,
+    dag=dag,
+)
